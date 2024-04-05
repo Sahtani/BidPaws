88ba41fd5bf9f492aa8bd48feae5745b9c5b0e15
Sahtani <soumia.sahtani@gmail.com>
2024-04-05 01:38:08 +0000

    Update danger-button component and web routes

--- BidPaws/routes/web.php
+++ BidPaws/routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AnnonceController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -41,3 +43,13 @@ Route::middleware(['auth'])->group(function () {
         Route::put('/annonces/{annonce}', [AnnonceController::class, 'confirm']);
     });
 });
+
+// Profile user
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
