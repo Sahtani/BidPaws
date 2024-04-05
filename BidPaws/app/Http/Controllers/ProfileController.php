17de6c15ee0a1dfe378167b1103b5eacbdd67029
Sahtani <soumia.sahtani@gmail.com>
2024-04-05 00:31:30 +0000

    Add function for  Update the user's profile information to ProfileController.php

--- BidPaws/app/Http/Controllers/ProfileController.php
+++ BidPaws/app/Http/Controllers/ProfileController.php
@@ -20,5 +20,20 @@ class ProfileController extends Controller
             'user' => $request->user(),
         ]);
     }
+        /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
 
 }
