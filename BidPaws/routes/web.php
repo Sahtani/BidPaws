800335338a12ebd8cd91ed49fe323dab39186b4e
Sahtani <soumia.sahtani@gmail.com>
2024-04-05 01:53:15 +0000

    Add PasswordController.php and modal.blade.php

--- BidPaws/routes/web.php
+++ BidPaws/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\AnnonceController;
+use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -50,6 +52,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 });
-
-require __DIR__.'/auth.php';
