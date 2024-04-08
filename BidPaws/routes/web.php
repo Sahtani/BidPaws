96ccd7660fd4aab287688580905d0277c2242173
Sahtani <soumia.sahtani@gmail.com>
2024-04-08 14:45:18 +0000

    Create categories table migration

--- BidPaws/routes/web.php
+++ BidPaws/routes/web.php
@@ -28,14 +28,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AnnonceController::class, 'recentAnnouncements'])->name('home');
 Route::get('/contact-us', function () {
     return view('contact-us');
-})->name('contact-us');Route::get('/sign-up', [AuthController::class, 'showRegistrationForm'])->name('sign-up');
-Route::get('/log-in', [AuthController::class, 'showLoginForm'])->name('log-in');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+})->name('contact-us');
+
+    Route::get('/sign-up', [AuthController::class, 'showRegistrationForm'])->name('sign-up');
+    Route::get('/log-in', [AuthController::class, 'showLoginForm'])->name('log-in');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth,user'])->group(function () {
     Route::prefix('user')->name('user.')->group(function () {
         Route::get('/create', [UserController::class, 'create'])->name('create');
         Route::get('/annonces', [AnnonceController::class, 'index']);
@@ -47,11 +49,16 @@ Route::middleware(['auth'])->group(function () {
     });
 });
 
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::prefix('admin')->name('admin.')->group(function () {
+    });
+});
+
 
 
 // Profile user
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','user'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
