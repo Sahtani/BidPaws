ded115ef57b4bda13348b3767326bf9351817e79
Sahtani <soumia.sahtani@gmail.com>
2024-04-08 09:51:44 +0000

    Added toggleAccess function to toggle user access

--- BidPaws/app/Http/Controllers/AdminController.php
+++ BidPaws/app/Http/Controllers/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function toggleAccess(User $user)
+    {
+        $user->update(['access' => !$user->access]);
+        return redirect()->back()->with('success', 'Access toggled successfully.');
+    }
 }
